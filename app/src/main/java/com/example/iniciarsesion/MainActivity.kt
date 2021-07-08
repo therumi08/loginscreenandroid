07de96113188885b01a7d7a5739dfb9c57d309d9
therumi08 <86691639+therumi08@users.noreply.github.com>
2021-07-08 03:01:16 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/iniciarsesion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iniciarsesion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
